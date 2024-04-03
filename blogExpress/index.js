bdae817e28ab182a5f3a71a61e6df5d05dd9ff02
NoTanmoyNotFound <tanmoydas78416@gmail.com>
2024-04-03 15:14:44 +0530

    start learning hbs

--- blogExpress/index.js
+++ blogExpress/index.js
@@ -1,12 +1,21 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const port = 3000
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const path    = require('path');
 
-app.use(express.static(path.join(__dirname,"static")))
-app.use('/',require(path.join(__dirname, 'routes/blog.js')))
+const app = express();
+const port = 3000;
 
+// Set up Handlebars view engine
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
-app.listen(port,() =>{
+// Serve static files from the "static" directory
+app.use(express.static(path.join(__dirname, "static")));
+
+// Mount routes
+app.use('/', require(path.join(__dirname, 'routes/blog.js')));
+
+// Start the server
+app.listen(port, () => {
     console.log(`Blog app listening at http://localhost:${port}`);
-})
+});
